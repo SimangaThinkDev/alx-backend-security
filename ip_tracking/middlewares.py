a7449bb02ac9a268c8f64d7bfbad11c7938e6390
Simangaliso Phakwe <siphakwjhb024@student.wethinkcode.co.za>
2025-09-04 17:42:23 +0200

    Done with Basic IP Logging Middleware

--- ip_tracking/middlewares.py
+++ ip_tracking/middlewares.py
@@ -1,10 +1,12 @@
 # ip_tracking/middleware.py
 
 from django.http import HttpRequest
+from .models import RequestLog
+from datetime import datetime
 
 LOG_FILE_PATH = "logs.txt"
 
-def simple_middleware(get_response):
+def log_requests(get_response):
     # One-time configuration and initialization.
 
     def middleware(request:HttpRequest):
@@ -21,9 +23,9 @@ def simple_middleware(get_response):
         body = request.body
         content_type = request.content_type
         files = request.FILES
+        path = request.path
 
-        # Code to be executed for each request/response after
-        # the view is called.
+        # So I have two loggers
 
         with open( LOG_FILE_PATH, "a" ) as logfile:
             logfile.write( f"""
@@ -36,10 +38,15 @@ def simple_middleware(get_response):
 [ ContentType ] : {content_type}
 [ Files ] : {files}
 """ )
+            
+        log_db = RequestLog( ip_address=ip, path=path )
+
+        log_db.save()
 
         return response
 
     return middleware
+
 
 def get_request_ip( request:HttpRequest ):
 
--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -3,5 +3,10 @@ from django.db import models
 class RequestLog( models.Model ):
 
     ip_address = models.CharField( max_length=40 )
-    timestamp = models.DateTimeField( auto_now_add=True )
     path = models.CharField( max_length=255 )
+    timestamp = models.DateTimeField( auto_now_add=True )
+
+    def __str__(self):
+        return f"Ip_Address: {self.ip_address} \n\
+            Timestamp: {self.timestamp} \n\
+            Path: {self.path}\n"
